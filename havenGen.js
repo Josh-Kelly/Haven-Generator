bcfcb5bd03416e9ab982bb48a70332080260ed47
Joshua Kelly <joshuakelly8@gmail.com>
2025-01-26 16:46:23 -0700

    first name generate commit

---
+++ havenGen.js
@@ -0,0 +1,59 @@
+// Function to generate a random name
+function generateName() {
+  const randomIndexA = Math.floor(Math.random() * firstName.length);
+  const randomIndexB = Math.floor(Math.random() * secondName.length);
+
+  const part1 = firstName[randomIndexA];
+  const part2 = secondName[randomIndexB];
+
+  // Example lists of names
+  const firstName = [
+    "Steam",
+    "Grub",
+    "Knuckle",
+    "Mammoth",
+    "Pale",
+    "Raven",
+    "Rust",
+    "Slate",
+    "Stone",
+    "Storm",
+    "Thorn",
+    "Tusk",
+    "Wolf",
+    "Wool",
+    "Worm",
+    "Wren",
+    "Yew",
+    "Roost",
+    "Rust",
+    "Spider",
+    "Surgeon",
+    "Turnip",
+  ];
+  const secondName = [
+    "falls",
+    "light",
+    "warren",
+    "bone",
+    "point",
+    "chase",
+    "feather",
+    "roost",
+    "kennel",
+    "field",
+    "gulch",
+    "rest",
+    "hall",
+    "seed",
+    "brook",
+    "vale",
+    "wood",
+    "grove",
+    "ridge",
+    "hollow",
+  ];
+  const randomName = generateName(part1, part2);
+
+  document.getElementById("havenName").innerHTML = randomName;
+}
